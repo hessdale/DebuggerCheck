b538cb4132690f44b54a1bd8aa509309da747b61
dale <hessdale96@gmail.com>
2023-03-16 17:44:45 -0600

    getting used to using the debugger

---
+++ js/app.js
@@ -0,0 +1,9 @@
+let username = "Dale";
+let user_age = 26;
+let is_member = true;
+
+if (username === "admin" || (user_age >= 30 && is_member === true)) {
+    console.log("Special Hello")
+} else {
+    console.log("Hello")
+}
