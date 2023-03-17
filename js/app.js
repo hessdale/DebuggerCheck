ac01910e0cd67fcbe32bfa97c49c56debe6db5f9
dale <hessdale96@gmail.com>
2023-03-16 20:08:42 -0600

    pop and push

--- js/app.js
+++ js/app.js
@@ -7,8 +7,9 @@ let current_volume = 50;
 let user_names = [`user1`, `user2`, `user3`, `user4`, `user5`];
 let user_ages = ['10', '20', '30', '40', '50'];
 let first_username = user_names[0];
-let last_user = user_names[4];
+
 let middle_user_age = user_ages[2];
+
 
 if (username === "admin" || (user_age >= 30 && is_member === true)) {
     console.log(`Special ${user_greeting}`);
@@ -19,4 +20,7 @@ console.log(`You are ${user_age}`);
 
 console.log(max_volume / current_volume);
 console.log(max_volume / current_volume * 100);
-
+user_names.push(`user6`);
+user_ages.push(`60`);
+let last_user_age = user_ages.pop();
+let last_user = user_names.pop();
