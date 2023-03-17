8ed20cf214edfea0b1473b4a37281d39614591aa
dale <hessdale96@gmail.com>
2023-03-16 18:52:34 -0600

    messing around with math

--- js/app.js
+++ js/app.js
@@ -1,9 +1,15 @@
-let username = "Dale";
+let username = "admin";
 let user_age = 26;
 let is_member = true;
+let user_greeting = `Hello ${username}`;
+let max_volume = 100;
+let current_volume = 50;
 
 if (username === "admin" || (user_age >= 30 && is_member === true)) {
-    console.log("Special Hello")
+    console.log(`Special ${user_greeting}`)
 } else {
-    console.log("Hello")
+    console.log(`${user_greeting}`)
 }
+console.log(`You are ${user_age}`)
+console.log(max_volume / current_volume)
+console.log(max_volume / current_volume * 100)
